dabdfa2c6d6008e569cbb6f609b0fe2f2a59fbf1
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-07 14:49:35 +0100

    Premiers tests pour récupérer les infos des films recherchés

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercice de parsage pour Datanaute</title>
+</head>
+<body>
+    <p>Entrez les informations que vous souhaitez rechercher</p>
+    <form method="post" action="recherche.php">
+        <input type="text" name="infos" id="infos" />
+        <input type="submit" name="button" id="button" value="Rechercher" />
+    </form>
+
+    <footer>Exercice réalisé par Enzo BEAUCHAMP</footer>
+</body>
+</html>
