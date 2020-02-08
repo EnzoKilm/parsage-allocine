f9003c2aa75408e68df9fc2c7f0459c65809ec6b
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-08 21:47:10 +0100

    Récupération films après recherche

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-#recherche = input("Recherche : ")
+recherche = input("Recherche : ")
+recherche = recherche.replace(" ", "+")
 
-#url = "http://www.allocine.fr/recherche/1/?q="
-url = 'http://www.allocine.fr/recherche/1/?q=star+wars'
+url = "http://www.allocine.fr/recherche/1/?q=" + recherche
 
 html = requests.get(url).text
 
@@ -15,13 +15,15 @@ soup = BeautifulSoup(html, 'lxml')
 div_films = soup.find('div', {'class': 'colcontent'})
 films = div_films.find_all('tr')
 
+code = """<html>
+<head></head>
+<body>"""
+
 for film in films:
     film.find_all('td')
     for td in film:
         try:
             classe = td.get('class')
-            #if(tg.get('class')) == ['totalWidth']:
-                #print(td)
         except:
             pass
 
@@ -32,10 +34,13 @@ for film in films:
                 pass
 
             if colspan == None:
-                print(td)
-                print("--------------------------------------------------")
+                code += str(td)
         else:
-            print(td)
-            print("--------------------------------------------------")
-            
+            code += str(td)
 
+code += """</body>
+</html>"""
+
+resultat = open('resultat.html','w', encoding='utf-8')
+resultat.write(code)
+resultat.close()
