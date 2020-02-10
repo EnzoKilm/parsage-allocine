f4575dfff5535e8a2bfd14968fad04d24a325fd0
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-10 19:40:08 +0100

    Ajout films par date

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
                 <option value="Films">film</option>
                 <option value="Stars">réalisateur</option>
                 <option value="Stars">acteur</option>
+                <option value="Date">date</option>
             </select>
             <input type="submit" name="button" id="button" value="Rechercher" />
         </form>
@@ -31,56 +32,88 @@
                 } else {
                     $infos = '';
                 }
-                
-                $mots = explode(" ", $infos);
-                $criteres = str_replace(" ", "+", $infos);
-                $url = "http://www.allocine.fr/recherche/?q=".$criteres;
-                
-                $html = file_get_html($url);
-                
-                $colcontent = $html->find('.colcontent');
 
-                $titlebars = $colcontent[0]->find('.titlebar');
-                foreach($titlebars as $titlebar) {
-                    $h2 = $titlebar->find('h2');
-                    if (strpos($h2[0], $select) !== false) {
-                        echo $h2[0];
-                        break;
+                if ($select == 'Date') {
+                    $decennie = substr($infos, 0, -1);
+                    $decennie .= '0';
+                    $url = 'http://www.allocine.fr/films/decennie-'.$decennie.'/annee-'.$infos;
+
+                    $html = file_get_html($url);
+
+                    $colmiddle = $html->find('.col-middle');
+
+                    $ul = $colmiddle[0]->find('ul');
+                    $films = $ul[1]->find('li');
+                    $position = 0;
+                    foreach($films as $film) {
+                        if ($film->class == 'mdl') {
+                            if ($position % 2 == 0) {
+                                echo '<div class="film';
+                                if ($position % 4 == 0) {
+                                    echo ' white">';
+                                } else {
+                                    echo ' gray">';
+                                }
+                                echo $film.'</div>';
+                            }
+                            $position += 1;
+                        }
                     }
-                }
+                } else {
+                    $mots = explode(" ", $infos);
+                    $criteres = str_replace(" ", "+", $infos);
+                    if ($select == 'Films') {
+                        $url = "http://www.allocine.fr/recherche/1/?q=".$criteres;
+                    } else {
+                        $url = "http://www.allocine.fr/recherche/?q=".$criteres;
+                    }
+                    
+                    $html = file_get_html($url);
+                    
+                    $colcontent = $html->find('.colcontent');
 
-                $divs = $colcontent[0]->find('div');
-                $spacer_before = false;
-                $div_found = false;
-                foreach($divs as $div) {
-                    if ($spacer_before == true) {
-                        $title_name = $div->find('h2');
-                        if ($title_name[0] == $h2[0]) {
-                            $div_found = true;
+                    $titlebars = $colcontent[0]->find('.titlebar');
+                    foreach($titlebars as $titlebar) {
+                        $h2 = $titlebar->find('h2');
+                        if (strpos($h2[0], $select) !== false) {
+                            echo $h2[0];
                             break;
                         }
                     }
 
-                    if ($div->class == 'spacer hrbicolor vmargin20t vmargin10b') {
-                        $spacer_before = true;
-                    } else {
-                        $spacer_before = false;
-                    }
-                }
-
-                $trs = $colcontent[0]->find('tr');
-                $position = 0;
-                foreach($trs as $tr) {
-                    if ($position % 2 == 0) {
-                        echo '<div class="film';
-                        if ($position % 4 == 0) {
-                            echo ' white">';
-                        } else {
-                            echo ' gray">';
+                    $divs = $colcontent[0]->find('div');
+                    $spacer_before = false;
+                    $div_found = false;
+                    foreach($divs as $div) {
+                        if ($spacer_before == true) {
+                            $title_name = $div->find('h2');
+                            if ($title_name[0] == $h2[0]) {
+                                $div_found = true;
+                                break;
+                            }
                         }
-                        echo $tr.'</div></div>';
+
+                        if ($div->class == 'spacer hrbicolor vmargin20t vmargin10b') {
+                            $spacer_before = true;
+                        } else {
+                            $spacer_before = false;
+                        }
                     }
-                    $position += 1;
+
+                    $trs = $colcontent[0]->find('tr');
+                    $position = 0;
+                    foreach($trs as $tr) {
+                        if ($position % 2 == 0) {
+                            echo '<div class="film';
+                            if ($position % 4 == 0) {
+                                echo ' white">';
+                            } else {
+                                echo ' gray">';
+                            }
+                            echo $tr.'</div></div>';
+                        }
+                        $position += 1;
+                    }
                 }
             ?>
         </div>
