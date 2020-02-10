b009ac7efba2068c8592dae4a48dd156cb99345a
EnzoKilm <enzo-beauchamp@live.fr>
2020-02-10 15:36:08 +0100

    Modifs

--- recherche.php
+++ recherche.php
@@ -6,19 +6,6 @@ $criteres = str_replace(" ", "+", $infos);
 $url = "http://www.allocine.fr/recherche/1/?q=".$criteres;
 echo $url;
 
-$page = file_get_contents($url);
-var_dump($page);
-
-echo '<hr/>';
-
-$doc = new DOMDocument();
-@$doc->loadHTML($page);
-
-// $doc_page = $doc->saveHTML();
-$tds = $doc->getElementsByTagName('td');
-foreach ($tds as $td) {
-    // var_dump($td->nodeValue, PHP_EOL);
-    // echo $td->nodeValue, PHP_EOL;
-}
+$html = file_get_html($url);
 
 // header("Location: index.php");
